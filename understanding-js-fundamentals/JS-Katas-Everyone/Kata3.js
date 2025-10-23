2230a1eefc9d76f354fc72f381df49c19e985824
PseudoMortus <johnathanc2359@pseudoservices.com>
2025-10-23 15:15:27 -0500

    Commit 3 for Kata 3

--- understanding-js-fundamentals/JS-Katas-Everyone/Kata3.js
+++ understanding-js-fundamentals/JS-Katas-Everyone/Kata3.js
@@ -1,13 +1,10 @@
-// Commit 2
-/* letting i start at 1, be no greater than 5, and increment each loop */
-for (let i = 1; i <= 5; i++) {
-/* Create an empty row */
+// Commit 3
+/* Change i to base_num to be clear what it is */
+for (let base_num = 1; base_num <= 5; base_num++) {
   let row = "";
-/* Create a loop that will effectively multiply i to make equal j */
-  for (let j = 1; j <= 5; j++) {
-/* Create a row with  each itteration of i's multiples into the row */
-    row += i * j + "\t";
+/* Change j to multiplied to be clear as well */
+  for (let multiplied = 1; multiplied <= 5; multiplied++) {
+    row += base_num * multiplied + "\t";
   }
-/* Print that row */
   console.log(row);
 }
