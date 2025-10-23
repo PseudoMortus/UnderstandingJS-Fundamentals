8afb759d06e9a061ccc790b5fc1d2c6532d9b613
PseudoMortus <johnathanc2359@pseudoservices.com>
2025-10-22 22:16:24 -0500

    Commit 2 for Kata 2

--- understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
+++ understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
@@ -1,13 +1,21 @@
-// Commit 1
-/* Focusing Now on the internal "command" */
-for (/* Same as Before */) {
-  if(/* where we're doing our calculations */) {
-    /* What to do if the calculation ends up being true*/
-  } else if (/* If something else is true instead */) {
-    /* What to do if the second option is true */
-  } else if (/* If something else instead is true */) {
-    /* What to do if that third thing is true */
-  } else{
-    /* What to do if none of that is true */
+// Commit 2
+/* Adding our loop, making 1 be from 1-20 and have it increment repeatedly until it can't */
+for (let i = 1; i <= 20; i++) {
+/* Checking if the result is BOTH divisible by 3 AND 5 evenly */
+  if (i % 3 === 0 && i % 5 === 0) {
+/* What to post in this sitch */
+    console.log("FizzBuzz");
+/* Checking if the result is divisible by 3 */
+  } else if (i % 3 === 0) {
+/* What to post in this sitch */
+    console.log("Fizz");
+/* Checking if the result is divisible by 5 */
+  } else if (i % 5 === 0) {
+/* What to post in this sitch */
+    console.log("Buzz");
+/* Checking if it's not divisible by either 3 or 5 */
+  } else {
+/* What to post in this sitch */
+    console.log(i);
   }
 }
