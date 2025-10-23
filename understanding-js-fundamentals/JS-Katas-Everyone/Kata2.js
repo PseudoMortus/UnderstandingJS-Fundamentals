416e3bed0b6b6411214f84109e175196a1bbc3a3
PseudoMortus <johnathanc2359@pseudoservices.com>
2025-10-22 22:23:32 -0500

    Commit 3 for Kata 2

--- understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
+++ understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
@@ -1,21 +1,17 @@
-// Commit 2
-/* Adding our loop, making 1 be from 1-20 and have it increment repeatedly until it can't */
-for (let i = 1; i <= 20; i++) {
-/* Checking if the result is BOTH divisible by 3 AND 5 evenly */
-  if (i % 3 === 0 && i % 5 === 0) {
-/* What to post in this sitch */
-    console.log("FizzBuzz");
-/* Checking if the result is divisible by 3 */
-  } else if (i % 3 === 0) {
-/* What to post in this sitch */
-    console.log("Fizz");
-/* Checking if the result is divisible by 5 */
-  } else if (i % 5 === 0) {
-/* What to post in this sitch */
-    console.log("Buzz");
-/* Checking if it's not divisible by either 3 or 5 */
+// Commit 3
+/* Use Number instead of I as we did Kata 1 */
+for (let number = 1; number <= 20; number++) {
+  if (number % 3 === 0 && number % 5 === 0) {
+/* Screw FizzBuzz, what is this, Bopit? We need to know what's going on, so let's do that. */
+    console.log("The number " + number + " is divisible by both 3 and 5.");
+  } else if (number % 3 === 0) {
+/* And again... */
+    console.log("The number " + number + " is divisible by 3, but not 5.");
+  } else if (number % 5 === 0) {
+/* And again... */
+    console.log("The number " + number + " is divisible by 5, but not 3.");
   } else {
-/* What to post in this sitch */
-    console.log(i);
+/* And again. */
+    console.log("The number " + number + " is not divisible by eiteher 3 nor 5");
   }
 }
