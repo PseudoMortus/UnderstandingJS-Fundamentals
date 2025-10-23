d7f159abe3715933f774fc1e1523aacc1859eeed
PseudoMortus <johnathanc2359@pseudoservices.com>
2025-10-22 22:13:34 -0500

    Commit 1 on Kata 2

--- understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
+++ understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
@@ -1,17 +1,13 @@
-// Kata 2: FizzBuzz from 1 to 20
-// ** OBJECTIVE** Create your own loop using the following logic**
-// Commit 1: "scaffold for loop"
-// Commit 2: "added fizzbuzz logic"
-// Commit 3: "refactored with clear variable and comments"
-
-for (let i = 1; i <= 20; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
+// Commit 1
+/* Focusing Now on the internal "command" */
+for (/* Same as Before */) {
+  if(/* where we're doing our calculations */) {
+    /* What to do if the calculation ends up being true*/
+  } else if (/* If something else is true instead */) {
+    /* What to do if the second option is true */
+  } else if (/* If something else instead is true */) {
+    /* What to do if that third thing is true */
+  } else{
+    /* What to do if none of that is true */
   }
 }
